9e501e4372c25bf2cd1d9a3159978a8801600bd0
Lucas Cavalheiro <l.cavalheirolc@gmail.com>
2026-02-13 18:02:00 -0300

    Adicionando estrutura inicial do backend

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express"
+
+const app = express();
+app.use(express.json());
+
+const usuarios = [
+    {id: 1, nome: "Lucas", idade: 24},
+    {id: 2, nome: "Marcela", idade: 44},
+    {id: 3, nome: "Thiago", idade: 45},
+    {id: 4, nome: "Fernanda", idade: 23},
+    {id: 5, nome: "Vítor", idade: 25},
+    {id: 6, nome: "José", idade: 24},
+    {id: 7, nome: "Marcelo", idade: 50},
+    {id: 8, nome: "Elizabeth", idade: 50},
+    {id: 9, nome: "Rafaela", idade: 19},
+    {id: 10, nome: "Bruno", idade: 3}
+]
+
+app.get("/usuarios", (request, response) => {
+    response.status(200).json(usuarios)
+})
+
+app.post("/usuarios", (request, response) => {
+    const {nome, idade} = request.body;
+
+    usuarios.push({idade, nome});    
+    response.status(201).json(usuarios)
+})
+
+app.put("/usuarios", (request, response) => {
+    const {nome, idade} = request.body;
+    const {id} = request.params;
+    const index = usuarios.findIndex(u => u.id == id);
+
+    if(index < 0){
+        return response.status(404).json("User Not Found :(");
+    }
+
+    usuarios[index] = {id: parseInt(id), nome, idade};    
+    response.status(201).json(usuarios);
+})
+
+app.delete("/usuarios", (request, response) => {
+    const {id} = request.params;
+    const index = usuarios.findIndex(u => u.id == id);
+
+    if(index < 0){
+        return response.status(404).json("User Not Found :(");
+    }
+
+    usuarios.splice(index, 1);    
+    response.status(200).json(usuarios);
+})
+
+app.listen(8080, () => {
+    console.log("Running on port 8080")
+})
