f6d986bebfebd4d2a9df6ccbc7249cea22db40a7
Lucas Cavalheiro <l.cavalheirolc@gmail.com>
2026-02-13 19:04:10 -0300

    update Index.js

--- index.js
+++ index.js
@@ -4,54 +4,64 @@ const app = express();
 app.use(express.json());
 
 const usuarios = [
-    {id: 1, nome: "Lucas", idade: 24},
-    {id: 2, nome: "Marcela", idade: 44},
-    {id: 3, nome: "Thiago", idade: 45},
-    {id: 4, nome: "Fernanda", idade: 23},
-    {id: 5, nome: "Vítor", idade: 25},
-    {id: 6, nome: "José", idade: 24},
-    {id: 7, nome: "Marcelo", idade: 50},
-    {id: 8, nome: "Elizabeth", idade: 50},
-    {id: 9, nome: "Rafaela", idade: 19},
-    {id: 10, nome: "Bruno", idade: 3}
+
+    {id: 1, nome: "Lucas", idade: 24, email: "Lu@email.com"},
+    {id: 2, nome: "Fernanda", idade: 23, email: "Nanda@email.com"},
+    {id: 3, nome: "Marcela", idade: 44, email: "Mar@email.com"},
+    {id: 4, nome: "Thiago", idade: 45, email: "Thi@email.com"},
+    {id: 5, nome: "Bruno", idade: 3, email: "Bebezinho@email.com"}
+
 ]
 
 app.get("/usuarios", (request, response) => {
+
     response.status(200).json(usuarios)
+
 })
 
 app.post("/usuarios", (request, response) => {
-    const {nome, idade} = request.body;
 
-    usuarios.push({idade, nome});    
+    const {nome, idade, email} = request.body;
+    usuarios.push({nome, idade, email});    
     response.status(201).json(usuarios)
+
 })
 
-app.put("/usuarios", (request, response) => {
-    const {nome, idade} = request.body;
+app.put("/usuarios/:id", (request, response) => {
+
+    const {nome, idade, email} = request.body;
     const {id} = request.params;
     const index = usuarios.findIndex(u => u.id == id);
 
     if(index < 0){
+
         return response.status(404).json("User Not Found :(");
+    
     }
 
-    usuarios[index] = {id: parseInt(id), nome, idade};    
-    response.status(201).json(usuarios);
+    usuarios[index] = {id: parseInt(id), nome, idade, email};    
+    response.status(200).json(usuarios);
+
 })
 
-app.delete("/usuarios", (request, response) => {
+app.delete("/usuarios/:id", (request, response) => {
+
     const {id} = request.params;
     const index = usuarios.findIndex(u => u.id == id);
 
     if(index < 0){
+
         return response.status(404).json("User Not Found :(");
+    
     }
 
     usuarios.splice(index, 1);    
     response.status(200).json(usuarios);
+
 })
 
 app.listen(8080, () => {
+
     console.log("Running on port 8080")
+
 })
