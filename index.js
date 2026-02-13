ecfba604a745c245d41c9bf2eb15b869d5efb97a
Lucas Cavalheiro <l.cavalheirolc@gmail.com>
2026-02-13 19:30:16 -0300

    corrigindo erros no PUT e POST

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ app.use(express.json());
 
 const usuarios = [
 
-    {id: 1, nome: "Lucas", idade: 24, email: "Lu@email.com"},
-    {id: 2, nome: "Fernanda", idade: 23, email: "Nanda@email.com"},
-    {id: 3, nome: "Marcela", idade: 44, email: "Mar@email.com"},
-    {id: 4, nome: "Thiago", idade: 45, email: "Thi@email.com"},
-    {id: 5, nome: "Bruno", idade: 3, email: "Bebezinho@email.com"}
+    {id: 1, nome: "Lucas", idade: 24},
+    {id: 2, nome: "Fernanda", idade: 23},
+    {id: 3, nome: "Marcela", idade: 44},
+    {id: 4, nome: "Thiago", idade: 45},
+    {id: 5, nome: "Bruno", idade: 3}
 
 ]
 
@@ -21,15 +21,15 @@ app.get("/usuarios", (request, response) => {
 
 app.post("/usuarios", (request, response) => {
 
-    const {nome, idade, email} = request.body;
-    usuarios.push({nome, idade, email});    
+    const {id, nome, idade} = request.body;
+    usuarios.push({id, nome, idade});    
     response.status(201).json(usuarios)
 
 })
 
 app.put("/usuarios/:id", (request, response) => {
 
-    const {nome, idade, email} = request.body;
+    const {nome, idade} = request.body;
     const {id} = request.params;
     const index = usuarios.findIndex(u => u.id == id);
 
@@ -39,7 +39,7 @@ app.put("/usuarios/:id", (request, response) => {
     
     }
 
-    usuarios[index] = {id: parseInt(id), nome, idade, email};    
+    usuarios[index] = {id: parseInt(id), nome, idade};    
     response.status(200).json(usuarios);
 
 })
